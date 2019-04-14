85bf01d5294ab9cf6fd995c133a923746a8bc53b
ChimengSoso <meng741258@gmail.com>
2019-04-14 21:30:44 +0700

    make LEC23_01.cpp

--- DAY23/LEC23_01.cpp
+++ DAY23/LEC23_01.cpp
@@ -7,7 +7,20 @@
 using namespace std;
 
 int main() {
-  freopen("input.txt", "r", stdin);
-  
+  // freopen("input.txt", "r", stdin);
+  string in, out = "";
+  while (getline(cin, in)) {
+    auto i = in.find(".......");
+    if (i == string::npos) { // not found '.......'
+      while (in.size() > 0 && in.back() == '\r')
+        in.pop_back();
+      out += (out.size() == 0 ? "" : " ") + in;
+    } else {
+      for (int k = 0; k < i; ++k)
+        out.push_back(in[k]);
+      break;
+    }
+  }
+  cout << out;
   return 0;
 }
