a96f1f6b6dcf275fb330654c12fbe625aaa9b8de
ChimengSoso <meng741258@gmail.com>
2019-04-14 18:55:02 +0700

    day 23 update

---
+++ DAY23/LEC23_01.cpp
@@ -0,0 +1,13 @@
+/**
+ *    author:  ChimengSoso
+ *    created: Sun Apr 14 2019 18:46:43
+ *    file:    LEC23_01.cpp
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  freopen("input.txt", "r", stdin);
+  
+  return 0;
+}
