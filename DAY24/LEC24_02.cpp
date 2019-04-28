ae44fdf92ffb6c182c54997626c322aecb53404c
ChimengSoso <meng741258@gmail.com>
2019-04-28 22:54:20 +0700

    do LEC24_02 got 80/100

--- DAY24/LEC24_02.cpp
+++ DAY24/LEC24_02.cpp
@@ -4,80 +4,88 @@
  *    file:    LEC24_02.cpp
 **/
 #include <bits/stdc++.h>
-#define mt make_tuple
-#define vi vector<int>
-
 using namespace std;
 
-using line = tuple<int, int, int, int>; // x1, y1, x2, y2 
-// type 'line' : mean line of point(x1, y1) to point(x2, y2)
+const double eps = 1e-6;
 
-using point = pair<int, int>;
+struct point {
+  double x, y;
+  point() {}
+  point(double x, double y) : x(x), y(y) {};
+  point(const point& s) : x(s.x), y(s.y) {};
+};
+
+struct line {
+  double a, b, c;
+  point ps, pe; // point start, point end
+  line() {}
+  line(point s, point t) {
+    ps = s, pe = t;
+    if (fabs(s.x - t.x) < eps)
+      a = 1.0, b = 0.0, c = -s.x;
+    else {
+      a = -(s.y - t.y) / (s.x - t.x);
+      b = 1.0;
+      c = (-a) * s.x - s.y;
+    }
+  }
+};
+
+bool isPrl(line L, line R) { // is parallel of 2 line
+  return fabs(L.a-R.a) < eps && fabs(L.b-R.b) < eps;
+}
+
+bool findIntersectPoint(line L, line R, point& p) {
+  if (isPrl(L, R)) return false;
+  p.x = (R.b * L.c - L.b*R.c) / (R.a * L.b - L.a * R.b);
+  if (fabs(L.b) < eps) L = R;
+  p.y = -(L.a * p.x + L.c);
+  return true;
+}
+
+bool inLine(line l, point p) {
+  point s(l.ps);
+  point e(l.pe);
+  double mnx = min(s.x, e.x);
+  double mny = min(s.y, e.y);
+  double mxx = max(s.x, e.x);
+  double mxy = max(s.y, e.y);
+  return (mnx < p.x or fabs(mnx-p.x) < eps) and (p.x < mxx or fabs(mxx-p.x) < eps) and 
+         (mny < p.y or fabs(mny-p.y) < eps) and (p.y < mxy or fabs(mxy-p.y) < eps);
+}
+
+bool intersect(line L, line R) {
+  point p;
+  bool isIntersect = findIntersectPoint(L, R, p);
+  if (!isIntersect) return false;
+  return inLine(L, p) and inLine(R, p);
+}
 
 int main() {
-  freopen("input.txt", "r", stdin);
+  // freopen("input.txt", "r", stdin);
   int tc; scanf("%d", &tc);
   while (tc--) {
-    int x1, y1, x2, y2, l, t, r, b;
-    scanf("%d%d%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &l, &t, &r, &b);
+    double psx, psy, pex, pey; // point start/end x/y
+    double x1, y1, x2, y2;
+    scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &psx, &psy, &pex, &pey, &x1, &y1, &x2, &y2);
 
-    function<int(point, point)> cross_pdct = [&](point P1, point P2) {
-      return P1.first * P2.second - P2.first * P1.second;
-    };
-    
-    function<int(point, point, point)> direction = [&](point p1, point p2, point p3) {
-      point sub31 = make_pair(p3.first - p1.first, p3.second - p1.second);
-      point sub21 = make_pair(p2.first - p1.first, p2.second - p2.second);
-      return cross_pdct(sub31, sub21);
-    };
+    double  xleft = min(x1, x2);
+    double   ytop = max(y1, y2);
+    double xright = max(x1, x2);
+    double  ydown = min(y1, y2);
 
-    function<bool(point, point, point)> on_segment = [&](point p1, point p2, point p)  {
-      bool b1 = min(p1.first, p2.first) <= p.first;
-      bool b2 = p.first <= max(p1.first, p2.first);
-      bool b3 = min(p1.second, p2.second) <= p.second;
-      bool b4 = p.second <= max(p1.second, p2.second);
-      return b1 && b2 && b3 && b4;
-    };
-    
-    function<bool(line, line)> intersect = [&](line L1, line L2) {
-      point p1 = make_pair(get<0>(L1), get<1>(L1));
-      point p2 = make_pair(get<2>(L1), get<3>(L1));
-      point p3 = make_pair(get<0>(L2), get<1>(L2));
-      point p4 = make_pair(get<2>(L2), get<3>(L2));
-      
-      int d1 = direction(p3, p4, p1);
-      int d2 = direction(p3, p4, p2);
-      int d3 = direction(p1, p2, p3);
-      int d4 = direction(p1, p2, p4);
+    line P(point(psx, psy), point(pex, pey));
+    line L(point(xleft, ytop), point(xleft, ydown));
+    line R(point(xright, ytop), point(xright, ydown));
+    line T(point(xleft, ytop), point(xright, ytop));
+    line D(point(xleft, ydown), point(xright, ydown));
 
-      if (((d1 > 0 and d2 < 0) or (d1 < 0 and d2 > 0)) and 
-          ((d3 > 0 and d4 < 0) or (d3 < 0 and d4 > 0)))
-            return true;
-      else if (d1 == 0 and on_segment(p3, p4, p1))
-        return true;
-      else if (d2 == 0 and on_segment(p3, p4, p2))
-        return true;
-      else if (d3 == 0 and on_segment(p1, p2, p3))
-        return true;
-      else if (d4 == 0 and on_segment(p1, p2, p4))
-        return true;
-      else
-        return false;
-    };
+    bool chk1 = intersect(P, L);
+    bool chk2 = intersect(P, R);
+    bool chk3 = intersect(P, T);
+    bool chk4 = intersect(P, D);
 
-    line real = mt(x1, y1, x2, y2);
-    
-    line bleft  = mt(l, t, l, b);
-    line bright = mt(r, t, r, b);
-    line btop   = mt(l, t, r, t);
-    line bdown  = mt(l, b, r, b);
-
-    bool chk_left  = intersect(real, bleft);
-    bool chk_right = intersect(real, bright);
-    bool chk_top   = intersect(real, btop);
-    bool chk_down  = intersect(real, bdown);
-    
-    bool rlt = chk_left || chk_right || chk_top || chk_down;
+    bool rlt = chk1 or chk2 or chk3 or chk4;
     printf("%c\n", rlt ? 'T' : 'F');
   }
   return 0;
