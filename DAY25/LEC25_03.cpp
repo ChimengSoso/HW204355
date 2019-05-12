249972c37e6f380f996394465dfe85a97ec43dea
ChimengSoso <meng741258@gmail.com>
2019-05-12 19:25:49 +0700

    do LEC25_03 got 30/100

--- DAY25/LEC25_03.cpp
+++ DAY25/LEC25_03.cpp
@@ -6,33 +6,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct point {
+  double x, y;
+  point() { x = y = 0.0; }
+  point(double x, double y) : x(x), y(y) {}
+};
+
+bool isInCircle(point p, point c, double r) {
+  // check point 'p' is in the circle center point 'c' with radius 'r'
+  double dy = p.y - c.y;
+  double dx = p.x - c.x;
+  double Euc = dx * dx + dy * dy;
+  double rSq = r * r;
+  return Euc <= rSq;
+}
+
+bool canBeCircle(point a, point b, double r, point& c) {
+  double d2 = hypot(a.x - b.x, a.y - b.y); d2 *= d2;
+  double det = r*r / d2 - 0.25;
+  if (det < 0.0) return false;
+  double h = sqrt(det);
+  c.x = (a.x + b.x)*0.5 + (a.y - b.y) * h;
+  c.y = (a.y + b.y)*0.5 + (b.x - a.x) * h;
+  return true;
+}
+
 int main() {
-  // freopen("input.txt", "r", stdin);
+  freopen("input.txt", "r", stdin);
   int tc; scanf("%d", &tc);
   while (tc--) {
-    char rlt = 'Y';
+    char rlt = 'N';
     int n; scanf("%d", &n);
     
-    using pdd = pair<double, double>;
-    
-    auto dst = [&](pdd a, pdd b) {
-      return (double)hypot(a.first-b.first, a.second-b.second);
-    };
-    
-    vector<pdd> p(n);
+    vector<point> p(n);
     for (int i = 0; i < n; ++i) {
       double x, y; scanf("%lf%lf", &x, &y);
-      p[i] = make_pair(x, y);  
+      p[i] = point(x, y);
     }
     
     double r;
-    scanf("%lf", &r); r *= 2.0;
-
+    scanf("%lf", &r);
     for (int i = 0; i < n-1; ++i) {
       for (int j = i+1; j < n; ++j) {
-        double d = dst(p[i], p[j]);
-        if (d > r) {
-          rlt = 'N'; break;
+        point c;
+        if (canBeCircle(p[i], p[j], r, c)) {
+          bool isAllInside = true;
+          for (int k = 0; k < n; ++k) {
+            if (!isInCircle(p[k], c, r)) {
+              isAllInside = false;
+              break;
+            }
+          }
+          if (isAllInside) {
+            rlt = 'Y';
+            break;
+          }
         }
       }
       if (rlt == 'N') break;
